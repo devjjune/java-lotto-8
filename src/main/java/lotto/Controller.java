aebf0f7e1c473e321281e08ee8ea95c28dc9770b
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-11-02 10:22:07 +0900

    feat(view, controller): 로또 발행 내역 출력 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -2,20 +2,21 @@ package lotto;
 
 import lotto.model.Lotto;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.List;
 
 public class Controller {
     public static void run() {
-        // [입력]
         String purchaseAmount = InputView.readPurchaseAmount();
-        String winningNumber = InputView.readWinningNumber();
-        String bonusNumber = InputView.readBonusNumber();
 
-        // [로또 발행]
         int lottoAmount = Lotto.calculateLottoAmount(purchaseAmount);
         List<Integer> randomNumbers = Lotto.pickRandomNumbers();
         List<List<Integer>> issuedLottos = Lotto.issueLottos(lottoAmount, randomNumbers);
+        OutputView.printIssuedLottos(issuedLottos);
+
+        String winningNumber = InputView.readWinningNumber();
+        String bonusNumber = InputView.readBonusNumber();
 
     }
 
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import java.util.List;
+
+public class OutputView {
+    public static void printIssuedLottos(List<List<Integer>> issuedLottos) {
+        int issuedAmount = issuedLottos.size();
+        System.out.println();
+        System.out.println(issuedAmount + "개를 구매했습니다.");
+        issuedLottos.forEach(lottos -> System.out.println(lottos));
+        System.out.println();
+
+    }
+
+}
